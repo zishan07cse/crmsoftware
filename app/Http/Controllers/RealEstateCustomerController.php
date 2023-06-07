2a63e05476a4e86fbee67300fa590b9eba4295fb
rizvee <zishan07cseceut@gmail.com>
2023-06-07 18:07:35 +0400

    todays final commit

--- app/Http/Controllers/RealEstateCustomerController.php
+++ app/Http/Controllers/RealEstateCustomerController.php
@@ -15,7 +15,20 @@ class RealEstateCustomerController extends Controller
 
         $realestate_customer = RealEstate_Customer::inRandomOrder()->where('status', '=', 0)->first();
         return view('realestate.callcustomer.customercallform', compact('realestate_customer'));
-        //dd($realestate_customer);
+    }
 
+    public function callinfosbmit()
+    {
+        echo "in called function ";
+    }
+
+    public function followup()
+    {
+        echo "in followup function";
+    }
+
+    public function meeting()
+    {
+        echo "in meeting funcion";
     }
 }
